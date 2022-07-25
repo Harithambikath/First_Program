8d20f936b74c5fd84d11e665296e48f3a660d9e2
Harithambika T H <harithambikat@gmail.com>
2022-07-26 00:49:13 +0530

    Added a Program - Operations.java

---
+++ Program/Operations.java
@@ -0,0 +1,18 @@
+class Operations
+{
+
+public void multiply(int val_1, int val_2)
+{
+	System.out.println(val_1* val_2);
+}
+
+public static void main(String args[])
+{
+	int a = 5;
+	int b = 10;
+	Operations os = new Operations();
+	os.multiply(a,b);
+
+}
+
+}
